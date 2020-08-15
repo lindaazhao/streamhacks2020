0a32c685cddae983ef7d2d1176ac476848464ff2
lindaazhao <linda.zhao23@gmail.com>
2020-08-14 23:07:30 -0400

    move img, remove jump and add score

--- maingame.py
+++ maingame.py
@@ -16,12 +16,16 @@ IVORY = (255, 255, 240)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 
 #Sets general variables
 key = 0
 start = 0
 level = 0
 paused = 0
+wasteSpawnTimer = 0
+score = 0
+font = pygame.font.SysFont('Arial', 25)
 
 #Sets graphics
 #lockscreen = pygame.image.load('Lock Screen.png')
@@ -30,11 +34,8 @@ paused = 0
 binpositionx = 320
 binpositiony = 410
 binvelocityx = 0
-binjump = 0
 bintype = "garbage"
-lidOpen = False
 binSucc = False
-
 
 #-----EVERYTHING WASTE-----
 class Waste:
@@ -62,23 +63,23 @@ waste_widths = [35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 
 waste_heights = [35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35]
 waste_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 wasteGraphics = [pygame.transform.scale(pygame.image.load('img/garb1.jpg'), (waste_widths[0], waste_heights[0]))]
-                 #pygame.transform.scale(pygame.image.load('img/garb2.jpg'), (waste_widths[1], waste_heights[1])),
-                 #pygame.transform.scale(pygame.image.load('img/garb3.jpg'), (waste_widths[2], waste_heights[2])),
-                 #pygame.transform.scale(pygame.image.load('img/garb4.jpg'), (waste_widths[3], waste_heights[3])),
-                 #pygame.transform.scale(pygame.image.load('img/garb5.jpg'), (waste_widths[4], waste_heights[4])),
-                 #pygame.transform.scale(pygame.image.load('img/garb6.jpg'), (waste_widths[5], waste_heights[5])),
-                 #pygame.transform.scale(pygame.image.load('img/garb7.jpg'), (waste_widths[6], waste_heights[6])),
-                 #pygame.transform.scale(pygame.image.load('img/garb8.jpg'), (waste_widths[7], waste_heights[7])),
-                 #pygame.transform.scale(pygame.image.load('img/garb9.jpg'), (waste_widths[8, waste_heights[8])),
-                 #pygame.transform.scale(pygame.image.load('img/garb10.jpg'), (waste_widths[9], waste_heights[9])),
-                 #pygame.transform.scale(pygame.image.load('img/garb11.jpg'), (waste_widths[10], waste_heights[10])),
-                 #pygame.transform.scale(pygame.image.load('img/garb12.jpg'), (waste_widths[11], waste_heights[11])),
-                 #pygame.transform.scale(pygame.image.load('img/garb13.jpg'), (waste_widths[12], waste_heights[12])),
-                 #pygame.transform.scale(pygame.image.load('img/garb14.jpg'), (waste_widths[13], waste_heights[13])),
-                 #pygame.transform.scale(pygame.image.load('img/garb15.jpg'), (waste_widths[14], waste_heights[14])),
-                 #pygame.transform.scale(pygame.image.load('img/garb16.jpg'), (waste_widths[15], waste_heights[15])),
-                 #pygame.transform.scale(pygame.image.load('img/garb17.jpg'), (waste_widths[16], waste_heights[16])),
-                 #pygame.transform.scale(pygame.image.load('img/garb18.jpg'), (waste_widths[17], waste_heights[17]))]
+                #pygame.transform.scale(pygame.image.load('img/garb2.jpg'), (waste_widths[1], waste_heights[1])),
+                #pygame.transform.scale(pygame.image.load('img/garb3.jpg'), (waste_widths[2], waste_heights[2])),
+                #pygame.transform.scale(pygame.image.load('img/garb4.jpg'), (waste_widths[3], waste_heights[3])),
+                #pygame.transform.scale(pygame.image.load('img/garb5.jpg'), (waste_widths[4], waste_heights[4])),
+                #pygame.transform.scale(pygame.image.load('img/garb6.jpg'), (waste_widths[5], waste_heights[5])),
+                #pygame.transform.scale(pygame.image.load('img/garb7.jpg'), (waste_widths[6], waste_heights[6])),
+                #pygame.transform.scale(pygame.image.load('img/garb8.jpg'), (waste_widths[7], waste_heights[7])),
+                #pygame.transform.scale(pygame.image.load('img/garb9.jpg'), (waste_widths[8, waste_heights[8])),
+                #pygame.transform.scale(pygame.image.load('img/garb10.jpg'), (waste_widths[9], waste_heights[9])),
+                #pygame.transform.scale(pygame.image.load('img/garb11.jpg'), (waste_widths[10], waste_heights[10])),
+                #pygame.transform.scale(pygame.image.load('img/garb12.jpg'), (waste_widths[11], waste_heights[11])),
+                #pygame.transform.scale(pygame.image.load('img/garb13.jpg'), (waste_widths[12], waste_heights[12])),
+                #pygame.transform.scale(pygame.image.load('img/garb14.jpg'), (waste_widths[13], waste_heights[13])),
+                #pygame.transform.scale(pygame.image.load('img/garb15.jpg'), (waste_widths[14], waste_heights[14])),
+                #pygame.transform.scale(pygame.image.load('img/garb16.jpg'), (waste_widths[15], waste_heights[15])),
+                #pygame.transform.scale(pygame.image.load('img/garb17.jpg'), (waste_widths[16], waste_heights[16])),
+                #pygame.transform.scale(pygame.image.load('img/garb18.jpg'), (waste_widths[17], waste_heights[17]))]
 
 #-------Title Screen-------
 while not done and start == 0:
@@ -113,20 +114,16 @@ while not done and start == 1:
                 key = "Right"
             if event.key == pygame.K_SPACE:
                 binSucc = True
-                velocityx = 0
-                lidOpen = True
-            if event.key == pygame.K_UP and binjump == 0:
-                binjump = 32 #Sets binjump variable to 32, which times the jump
             if event.key == pygame.K_a: #Bin swap controls
                 bintype = "garbage"
             if event.key == pygame.K_s:
                 bintype = "compost"
             if event.key == pygame.K_d:
                 bintype = "recycling"
-            if event.key == pygame.K_w:
-                newWaste = Waste(1, random.randint(250,665), 50, 2, 35, 35)
-                waste_list.append(newWaste)
-                print(waste_list)
+            #if event.key == pygame.K_w:
+                #newWaste = Waste(1, random.randint(250,665), 50, 2, 35, 35)
+                #waste_list.append(newWaste)
+                #print(waste_list)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT: #Bin movement controls
                 if key == "Left":
@@ -137,25 +134,31 @@ while not done and start == 1:
             if event.key == pygame.K_SPACE:
                 binSucc = False
 
+    #Bin succ ----
     if binSucc:
       velocityx = 0
       for i in range(len(waste_list)):
         waste = waste_list[i]
         if abs(waste.x+(waste.width/2) - (binpositionx + 30)) <= 30:
           waste.y += 3
-          if waste.y >= binpositiony:
+          if waste.y >= binpositiony and waste.y <= (binpositiony + 40):
             remove_waste.append(waste)
+            score += 1
       for i in remove_waste:
         if i in waste_list:
           waste_list.remove(i)
-
+    
     #Bin movement ---
-    if key == "Left": #Adds velocity leftward
+    if key == "Left" and binSucc == False: #Adds velocity leftward, normal speed
         binvelocityx += -1.2
-    if key == "Right": #Adds velocity rightward
+    if key == "Right" and binSucc == False: #Adds velocity rightward, normal speed
         binvelocityx += 1.2
+    if key == "Left" and binSucc == True: #Adds velocity leftward, succ speed
+        binvelocityx += -0.95
+    if key == "Right" and binSucc == True: #Adds velocity rightward, succ speed
+        binvelocityx += 0.95
 
-    if binvelocityx >= -1 and binvelocityx <= 1: #Velocity decays if already cloe to 0
+    if binvelocityx >= -0.9 and binvelocityx <= 0.9: #Velocity decays if already cloe to 0
         binvelocityx = 0
     if binvelocityx > 0: #Velocity decays if positive
         binvelocityx -= 0.9
@@ -168,42 +171,32 @@ while not done and start == 1:
     if binpositionx > 630 and binvelocityx > 0: #Bin sprite does not move past the right edge of the screen
         binpositionx = 630
 
-    if binjump <= 32 and binjump >= 29: #Bin jumps up
-        binpositiony -= 13
-        binjump -= 1
-    if binjump <= 28 and binjump >= 25:
-        binpositiony -= 9
-        binjump -= 1
-    if binjump <= 24 and binjump >= 21:
-        binpositiony -= 6
-        binjump -= 1
-    if binjump <= 20 and binjump >= 17:
-        binpositiony -= 4
-        binjump -= 1
-
-    if binjump <= 16 and binjump >= 13: #Bin falls down
-        binpositiony += 4
-        binjump -= 1
-    if binjump <= 12 and binjump >= 9:
-        binpositiony += 6
-        binjump -= 1
-    if binjump <= 8 and binjump >= 5:
-        binpositiony += 9
-        binjump -= 1
-    if binjump <= 4 and binjump >= 1:
-        binpositiony += 13
-        binjump -= 1
-
     #Bin swap –––
     if key == "Garbage": #Swaps bin to garbage
         bintype = "garbage"
-    if key == "Compost": #Swaps bin to garbage
+    if key == "Compost": #Swaps bin to compost
         bintype = "compost"
+    if key == "Recycling": #Swaps bin to recycling
+        bintype = "recycling"
 
+    #Waste spawn ---
+    wasteSpawnTimer += 1
+    if wasteSpawnTimer == 120:
+      newWaste = Waste(1, random.randint(250, 655), 0, 2, 35, 35)
+      waste_list.append(newWaste)
+      wasteSpawnTimer = 0
+  
     #Draws in the background sky and grass
     screen.fill(SKY)
     pygame.draw.rect(screen, GRASS, (0, 490, 960, 50), 0)
     pygame.draw.rect(screen, IVORY, (700, 0, 260, 960))
+    tracker = font.render(str(score), 1, BLACK)
+    screen.blit(tracker, (900, 500))
+    
+    for i in range(len(waste_list)):
+      waste = waste_list[i]
+      waste.y += waste.speed
+      waste.draw()
 
     #Draw bin sprite –––
     if bintype == "garbage":
@@ -212,11 +205,6 @@ while not done and start == 1:
         pygame.draw.rect(screen, GREEN, [binpositionx, binpositiony, int(60), int(80)], int(0))
     if bintype == "recycling":
         pygame.draw.rect(screen, BLUE, [binpositionx, binpositiony, int(60), int(80)], int(0))
-    
-    for i in range(len(waste_list)):
-      waste = waste_list[i]
-      waste.y += waste.speed
-      waste.draw()
 
     #Updates screen
     pygame.display.flip()
