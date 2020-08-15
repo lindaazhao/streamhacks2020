5e050f50874e05935b126dead5ffb0979ff5681e
lindaazhao <linda.zhao23@gmail.com>
2020-08-15 14:54:51 -0400

    updated in game screens, placeholder sounds?

--- maingame.py
+++ maingame.py
@@ -31,10 +31,30 @@ score_threshold = 15
 font = pygame.font.SysFont('Arial', 25)
 
 # Gameplay music
-# mixer.music.load('fallguys.mp3')
-# mixer.music.play(-1)
+mixer.music.load('sound/fallguys.mp3')
+mixer.music.play(-1)
 
-# Sets graphics
+# Sound effect
+succ_sound = mixer.Sound("sound/laserbeam.wav")
+switch_sound = mixer.Sound("sound/switch.wav")
+levelup_sound = mixer.Sound("sound/levelup.wav")
+negative_sound = mixer.Sound("sound/negative.wav")
+intake_sound = mixer.Sound("sound/intake.wav")
+move_sound = mixer.Sound("sound/grass.wav")
+
+# Sets graphics for bins
+
+garbagebot = pygame.transform.scale(pygame.image.load('img/bins/garbagebot.png'), (60, 80))
+garbagetopclose = pygame.transform.scale(pygame.image.load('img/bins/garbagetopclose.png'), (60, 13))
+garbagetopopen = pygame.transform.scale(pygame.image.load('img/bins/garbagetopopen.png'), (60, 50))
+compostbot = pygame.transform.scale(pygame.image.load('img/bins/compostbot.png'), (60, 80))
+composttopclose = pygame.transform.scale(pygame.image.load('img/bins/composttopclose.png'), (60, 13))
+composttopopen = pygame.transform.scale(pygame.image.load('img/bins/composttopopen.png'), (60, 50))
+recyclebot = pygame.transform.scale(pygame.image.load('img/bins/recyclebot.png'), (60, 80))
+recycletopclose = pygame.transform.scale(pygame.image.load('img/bins/recycletopclose.png'), (60, 13))
+recycletopopen = pygame.transform.scale(pygame.image.load('img/bins/recycletopopen.png'), (60, 50))
+
+# Sets graphics for screens
 lockscreen = pygame.image.load('img/screens/Lock Screen.png')
 level1 = pygame.image.load('img/screens/Level 1.png')
 level2 = pygame.image.load('img/screens/Level 2.png')
@@ -62,10 +82,7 @@ class Waste:
         self.height = wasteHeight
 
     def draw(self):
-        if self.ID == 1:
-            wasteType = wasteGraphics[self.ID - 1]
-        if self.ID == 2:
-            wasteType = wasteGraphics[self.ID - 1]
+        wasteType = wasteGraphics[self.ID - 1]
         screen.blit(wasteType, (int(self.x), int(self.y)))
 
 
@@ -73,29 +90,36 @@ waste_list = []
 remove_waste = []
 waste_pool = [1, 2]  # List of waste items eligible to be spawned in the round
 waste_randomizer = 0  # A random number drawn from waste_pool
-waste_widths = [35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35]
-waste_heights = [35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35]
+waste_widths = [40, 50, 50, 50, 50, 40, 50, 30, 50, 50, 50, 48, 40, 40, 20, 20, 40, 23]
+waste_heights = [40, 25, 25, 25, 31, 40, 25, 45, 25, 30, 40, 45, 40, 40, 40, 40, 40, 40]
 waste_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-wasteGraphics = [pygame.transform.scale(pygame.image.load('img/waste/garb1.png'), (waste_widths[0], waste_heights[0]))]
+wasteGraphics = [pygame.transform.scale(pygame.image.load('img/waste/garb1.png'), (waste_widths[0], waste_heights[0])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb2.png'), (waste_widths[1], waste_heights[1])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb3.png'), (waste_widths[2], waste_heights[2])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb4.png'), (waste_widths[3], waste_heights[3])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb5.png'), (waste_widths[4], waste_heights[4])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb6.png'), (waste_widths[5], waste_heights[5])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb7.png'), (waste_widths[6], waste_heights[6])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb8.png'), (waste_widths[7], waste_heights[7])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb9.png'), (waste_widths[8], waste_heights[8])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb10.png'), (waste_widths[9], waste_heights[9])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb11.png'),
+                                        (waste_widths[10], waste_heights[10])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb12.png'),
+                                        (waste_widths[11], waste_heights[11])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb13.png'),
+                                        (waste_widths[12], waste_heights[12])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb14.png'),
+                                        (waste_widths[13], waste_heights[13])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb15.png'),
+                                        (waste_widths[14], waste_heights[14])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb16.png'),
+                                        (waste_widths[15], waste_heights[15])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb17.png'),
+                                        (waste_widths[16], waste_heights[16])),
+                 pygame.transform.scale(pygame.image.load('img/waste/garb18.png'),
+                                        (waste_widths[17], waste_heights[17]))]
 
-
-# pygame.transform.scale(pygame.image.load('img/waste/garb2.jpg'), (waste_widths[1], waste_heights[1])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb3.jpg'), (waste_widths[2], waste_heights[2])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb4.jpg'), (waste_widths[3], waste_heights[3])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb5.jpg'), (waste_widths[4], waste_heights[4])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb6.jpg'), (waste_widths[5], waste_heights[5])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb7.jpg'), (waste_widths[6], waste_heights[6])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb8.jpg'), (waste_widths[7], waste_heights[7])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb9.jpg'), (waste_widths[8, waste_heights[8])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb10.jpg'), (waste_widths[9], waste_heights[9])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb11.jpg'), (waste_widths[10], waste_heights[10])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb12.jpg'), (waste_widths[11], waste_heights[11])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb13.jpg'), (waste_widths[12], waste_heights[12])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb14.jpg'), (waste_widths[13], waste_heights[13])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb15.jpg'), (waste_widths[14], waste_heights[14])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb16.jpg'), (waste_widths[15], waste_heights[15])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb17.jpg'), (waste_widths[16], waste_heights[16])),
-# pygame.transform.scale(pygame.image.load('img/waste/garb18.jpg'), (waste_widths[17], waste_heights[17]))]
 
 # -----EVERYTHING CLOUDS-----
 class Cloud:
@@ -155,13 +179,18 @@ while not done and start == 1:
                 key = "Right"
             if event.key == pygame.K_SPACE:
                 binSucc = True
-                # laser_sound = mixer.Sound("laserbeam.wav")
-                # laser_sound.play()
+                succ_sound.play()
             if event.key == pygame.K_a:  # Bin swap controls
+                if bintype != "garbage":
+                    switch_sound.play()
                 bintype = "garbage"
             if event.key == pygame.K_s and level >= 2:
+                if bintype != "compost":
+                    switch_sound.play()
                 bintype = "compost"
             if event.key == pygame.K_d and level >= 3:
+                if bintype != "recycling":
+                    switch_sound.play()
                 bintype = "recycling"
 
         if event.type == pygame.KEYUP:
@@ -173,7 +202,7 @@ while not done and start == 1:
                     key = 0
             if event.key == pygame.K_SPACE:
                 binSucc = False
-                # laser_sound.stop()
+                succ_sound.stop()
 
     # Bin succ ----
     if binSucc:
@@ -182,14 +211,19 @@ while not done and start == 1:
             if abs(waste.x + (waste.width / 2) - (
                     binpositionx + 30)) <= 30:  # Checks if waste x-position is within range of the bin
                 waste.y += 3
-                if (bintype == "garbage" and waste.ID in range(1, 6)) or (
-                        bintype == "compost" and waste.ID in range(7, 12)) or (
-                        bintype == "recycling" and waste.ID in range(13,
-                                                                     18)):  # Checks if waste type corresponds to bin type
-                    if waste.y >= binpositiony and waste.y <= (
-                            binpositiony + 40):  # Checks if waste y-position is within range of the bin
-                        remove_waste.append(waste)
+                if waste.y >= binpositiony and waste.y <= (
+                        binpositiony + 40):  # Checks if waste y-position is within range of the bin
+                    remove_waste.append(waste)
+                    intake_sound.play()
+                    if (bintype == "garbage" and waste.ID in range(1, 7)) or (
+                            bintype == "compost" and waste.ID in range(7, 13)) or (
+                            bintype == "recycling" and waste.ID in range(13,
+                                                                         19)):  # Checks if waste type corresponds to bin type
                         score += 1
+                    else:
+                        if score > 0:
+                            score -= 1
+                            switch_sound.negative()
         for i in remove_waste:
             if i in waste_list:
                 waste_list.remove(i)
@@ -235,7 +269,7 @@ while not done and start == 1:
         wasteSpawnTimer += 1
         if wasteSpawnTimer == 120:
             waste_randomizer = random.choice(waste_pool)
-            newWaste = Waste(1, random.randint(10, 650), 0, random.randint(3, 4 + level) / 3, 35,
+            newWaste = Waste(waste_randomizer, random.randint(10, 650), 0, random.randint(3, 3 + level) / 4, 35,
                              35)  # Change this later
             waste_list.append(newWaste)
             wasteSpawnTimer = 0
@@ -249,48 +283,52 @@ while not done and start == 1:
         cloudSpawnTimer = 0
 
     # Level up system
-    if level == 1 and score >= 15:
+    if level == 1 and score >= 1:
         score = 0  # Resets the score for the next threshold
         level += 1  # Increments the level
         paused = 1  # Pauses the game
         score_threshold = 15  # Sets the new score threshold
         waste_pool.extend((7, 8))  # Adds in compost items
+        levelup_sound.play()  # Sound effect for levelling up
         for i in range(len(waste_list)):  # Deletes all current trash items
             waste = waste_list[i]
             remove_waste.append(waste)
         for i in remove_waste:
             if i in waste_list:
                 waste_list.remove(i)
-    if level == 2 and score >= 15:  # Simile
+    if level == 2 and score >= 2:  # Simile
         score = 0
         level += 1
         paused = 1
         score_threshold = 15
         waste_pool.extend((13, 14))  # Adds in recycling items
+        levelup_sound.play()  # Sound effect for levelling up
         for i in range(len(waste_list)):
             waste = waste_list[i]
             remove_waste.append(waste)
         for i in remove_waste:
             if i in waste_list:
                 waste_list.remove(i)
-    if level == 3 and score >= 15:
+    if level == 3 and score >= 2:
         score = 0
         level += 1
         paused = 1
         score_threshold = 20
         waste_pool.extend((3, 4, 9, 10, 15, 16))  # Adds in general items
+        levelup_sound.play()  # Sound effect for levelling up
         for i in range(len(waste_list)):
             waste = waste_list[i]
             remove_waste.append(waste)
         for i in remove_waste:
             if i in waste_list:
                 waste_list.remove(i)
-    if level == 4 and score >= 20:
+    if level == 4 and score >= 3:
         score = 0
         level += 1
         paused = 1
         score_threshold = 30
         waste_pool.extend((5, 6, 11, 12, 17, 18))  # Adds in general items
+        levelup_sound.play()  # Sound effect for levelling up
         for i in range(len(waste_list)):
             waste = waste_list[i]
             remove_waste.append(waste)
@@ -302,6 +340,7 @@ while not done and start == 1:
         score = 0
         level += 1
         paused = 1
+        levelup_sound.play()  # Sound effect for levelling up
         for i in range(len(waste_list)):
             waste = waste_list[i]
             remove_waste.append(waste)
@@ -333,19 +372,42 @@ while not done and start == 1:
         if i in cloud_list:
             cloud_list.remove(i)
 
+    # Draw bin sprite layers that are behind the waste items
+    if bintype == "garbage":
+        if binSucc:
+            screen.blit(garbagetopopen, (binpositionx, binpositiony - 50))
+        else:
+            screen.blit(garbagebot, (binpositionx, binpositiony))
+            screen.blit(garbagetopclose, (binpositionx, binpositiony - 13))
+    if bintype == "compost":
+        if binSucc:
+            screen.blit(composttopopen, (binpositionx, binpositiony - 50))
+        else:
+            screen.blit(compostbot, (binpositionx, binpositiony))
+            screen.blit(composttopclose, (binpositionx, binpositiony - 13))
+    if bintype == "recycling":
+        if binSucc:
+            screen.blit(recycletopopen, (binpositionx, binpositiony - 50))
+        else:
+            screen.blit(recyclebot, (binpositionx, binpositiony))
+            screen.blit(recycletopclose, (binpositionx, binpositiony - 13))
+
     # Draw in and apply gravity to the waste items
     for i in range(len(waste_list)):
         waste = waste_list[i]
         waste.y += waste.speed
         waste.draw()
 
-    # Draw bin sprite
+    # Draw bin sprite layers that are in front of the waste items
     if bintype == "garbage":
-        pygame.draw.rect(screen, RED, [binpositionx, binpositiony, int(60), int(80)], int(0))
+        if binSucc:
+            screen.blit(garbagebot, (binpositionx, binpositiony))
     if bintype == "compost":
-        pygame.draw.rect(screen, GREEN, [binpositionx, binpositiony, int(60), int(80)], int(0))
+        if binSucc:
+            screen.blit(compostbot, (binpositionx, binpositiony))
     if bintype == "recycling":
-        pygame.draw.rect(screen, BLUE, [binpositionx, binpositiony, int(60), int(80)], int(0))
+        if binSucc:
+            screen.blit(recyclebot, (binpositionx, binpositiony))
 
     # Draw HUD
     pygame.draw.rect(screen, IVORY, (700, 0, 260, 960))
